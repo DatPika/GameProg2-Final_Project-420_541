371a7bc2ed78912092353433581ab5ded5650e89
DatPika <89601821+DatPika@users.noreply.github.com>
2024-12-04 23:05:57 -0500

    tiny map collider fixes + build created + cursor hidden

---
+++ Assets/Scripts/CursorManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class CursorScript : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        //Set Cursor to not be visible
+        Cursor.visible = false;
+        
+        // Perhaps will enable later for inventory and such
+    }
+}
