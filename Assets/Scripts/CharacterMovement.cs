09768e3df4652e4f6985f2d0c5a34dcc094763a5
hedi3489 <hedibelh4@gmail.com>
2024-12-21 18:20:16 -0500

    obelisk interaction complete

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// imports needed for UI
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CharacterMovement : MonoBehaviour
 {
@@ -21,9 +24,13 @@ public class CharacterMovement : MonoBehaviour
     private CharacterController controller;
     private Animator animator;
     
+    // Variable needed for UI + mechanics
     public GameObject MessagePanel;
-    // public GameObject Cube;
-    // private bool cubePickup = true;
+    public Text MessageText;
+    public GameObject obelisk;
+    public bool invisibility = false;
+    public bool erase = false;
+    public bool interaction = true;
 
     private void Start()
     {
@@ -40,6 +47,9 @@ public class CharacterMovement : MonoBehaviour
             ProcessMovement();
         }
         ProcessGravity();
+        InteractWithObelisk();
+        
+        
     }
   
     void ProcessMovement()
@@ -102,21 +112,101 @@ public class CharacterMovement : MonoBehaviour
     }
 
     // Methods for showing/hiding MessagePanel
-    public void OpenMessagePanel(){
+    public void OpenMessagePanel(string message, float delay)
+    {
+        // Start a coroutine to open the panel after the delay
+        StartCoroutine(OpenPanelAfterDelay(message, delay));
+    }
+
+    // Coroutine to handle the delay and open the panel
+    private IEnumerator OpenPanelAfterDelay(string message, float delay)
+    {
+        // Wait for the specified delay time
+        yield return new WaitForSeconds(delay);
+
+        // After the delay, set the message and activate the MessagePanel
+        MessageText.text = message;
         MessagePanel.SetActive(true);
     }
-    public void CloseMessagePanel(){
+
+    public void CloseMessagePanel(float delay)
+    {
+        StartCoroutine(ClosePanelAfterDelay(delay));
+    }
+
+    // Coroutine to handle the delay
+    private IEnumerator ClosePanelAfterDelay(float delay)
+    {
+        // Wait for the specified delay time
+        yield return new WaitForSeconds(delay);
+
+        // After the delay, close the MessagePanel
         MessagePanel.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other){
-        if(other.CompareTag("Cube")){
-            OpenMessagePanel();
+        if(other.CompareTag("Obelisk")){
+            OpenMessagePanel("- Press E to interact -", 0f);
         }
     }
     private void OnTriggerExit(Collider other){
-        if(other.CompareTag("Cube")){
-            CloseMessagePanel();
+        if(other.CompareTag("Obelisk")){
+            CloseMessagePanel(0f);
+        }
+    }
+
+    private void InteractWithObelisk(){
+        // Getting scene name to decide what ability to grant
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        if(MessagePanel.activeSelf){
+            Debug.Log("Scene Active...");
+            // Level 1
+            if(sceneName == "Intro-Hike"){
+                Debug.Log("Entering Invisibility Interaction...");
+                // If E is pressed while the Message Panel is shown
+                //      Invisibility is unlocked, then interactions are blocked.
+                //      User invisibility bool in invis. method.
+                if(Input.GetKeyDown(KeyCode.E)){
+                    invisibility = true;
+                    interaction = false;
+
+                    // Disabling obelisk collider to avoid further interaction
+                    //      and MessagePanel opening and closing
+                    Debug.Log("Disabling Obelisk collider");
+                    obelisk.GetComponent<Collider>().enabled = false;
+
+                    // User friendly UI
+                    OpenMessagePanel("- Invisibility Obtained -", 0f);
+                    CloseMessagePanel(3f);
+                    OpenMessagePanel("- Press X to turn Invisible -", 3.5f);
+                    CloseMessagePanel(6.5f);
+                }
+            }
+
+            // Level 2
+            if(sceneName == "Scene2"){
+                Debug.Log("Entering Erase Interaction...");
+                // If E is pressed while the Message Panel is shown
+                //      Erase is unlocked, then interactions are blocked.
+                //      User erase bool in invis method.
+                if(Input.GetKeyDown(KeyCode.E)){
+                    erase = true;
+                    interaction = false;
+
+                    // Disabling obelisk collider to avoid further interaction
+                    //      and MessagePanel opening and closing
+                    Debug.Log("Disabling Obelisk collider");
+                    obelisk.GetComponent<Collider>().enabled = false;
+
+                    // User friendly UI
+                    OpenMessagePanel("- Erase Obtained -", 0f);
+                    CloseMessagePanel(3f);
+                    OpenMessagePanel("- Press X to execute Erase -", 3.5f);
+                    CloseMessagePanel(6.5f);
+                }
+            }
         }
     }
 }
