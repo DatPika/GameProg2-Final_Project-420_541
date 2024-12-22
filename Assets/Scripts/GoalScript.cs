aa041757b86282853cb81bffa508aa477fe33ac9
hedi3489 <hedibelh4@gmail.com>
2024-12-21 20:48:15 -0500

    level 1 complete

---
+++ Assets/Scripts/GoalScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+}
+
